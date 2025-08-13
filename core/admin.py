29d06f0a3802e1806293981f4444fa145234b581
Rafael <rafaellevi@gmail.com>
2025-08-13 11:48:46 -0300

    add routers admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from salao.models import profissional, agendamento, cliente,servico
+from django.contrib.auth.models import Group, User
 
 class CustomAdminSite(admin.AdminSite):
     site_header = "Salão de beleza backoffice"
@@ -9,3 +10,5 @@ admin_site.register(profissional.Profissional)
 admin_site.register(agendamento.Agendamento)
 admin_site.register(cliente.Cliente)
 admin_site.register(servico.Servico)
+admin_site.register(User)
+admin_site.register(Group)
