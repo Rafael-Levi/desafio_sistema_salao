b6bc66a0c9e79ccaf0c9fa4fa83925f405bab8f1
Rafael <rafaellevi@gmail.com>
2025-08-14 18:24:17 -0300

    update

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+f6=r7ff!+rfxgp!i-l(yflsqp$-b8uv4kpl5=!)*(v$sjyra'
+SECRET_KEY = os.environ.get("SECRET_KEY", default="secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ['.localhost','127.0.0.1','[::1]']
+ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
--- salao/routers/login_urls.py
+++ salao/routers/login_urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
+from ..views.logout import logout_user
 app_name = 'login'
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login:login'), name='logout'),
+    path('logout/', logout_user, name='logout' )
 ]
