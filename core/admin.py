e2cd8e25f33dd38c527cf7f588d3831e237ac0ec
Rafael Levi <66269075+Rafael-Levi@users.noreply.github.com>
2025-09-21 12:13:39 -0300

    fixing user pass

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
-from salao.models import profissional, agendamento, cliente,servico
 from django.contrib.auth.models import Group, User
+from django.contrib.auth.admin import UserAdmin
+from salao.models import profissional, agendamento, cliente, servico
 
 class CustomAdminSite(admin.AdminSite):
     site_header = "Salão de beleza backoffice"
 
 admin_site = CustomAdminSite()
+
 admin_site.register(profissional.Profissional)
 admin_site.register(agendamento.Agendamento)
 admin_site.register(cliente.Cliente)
 admin_site.register(servico.Servico)
-admin_site.register(User)
+
+admin_site.register(User, UserAdmin)
 admin_site.register(Group)
